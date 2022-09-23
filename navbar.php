7ec40539132d00176b9739ddde66343d6c951239
yash aher <aheryash2004@gmail.com>
2022-09-23 15:06:10 +0530

    yash

---
+++ navbar.php
@@ -0,0 +1,81 @@
+<div class="nav-container"> 
+        <nav > 
+            <ul class="mobile-nav">
+                <li>
+                    <div class="menu-icon-container">
+                        <div class="menu-icon">
+                            <span class="line-1"></span>
+                            <span class="line-2"></span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="index.html" class="link-logo">Guru Mobiles</a>
+                </li>
+                <li>
+                    <a href="#" class="link-bag"></a>
+                </li>
+
+            </ul>
+            <ul class="desktop-nav ">
+                <li>
+                    <a href="index.html" class="link-logo">Guru Mobiles</a>
+                </li>
+                <li class="samsung">
+                    <a href="#">Samsung</a>
+                </li>
+                <li>
+                    <a href="#">OnePlus</a>
+                </li>
+                <li>
+                    <a href="#">Oppo</a>
+                </li>
+                <li>
+                    <a href="#">Vivo</a>
+                </li>
+                <li>
+                    <a href="#">Xiaomi</a>
+                </li>
+                <li>
+                    <a href="#">Realme</a>
+                </li>
+                <li>
+                    <a href="#">Apple</a>
+                </li>
+                <li>
+                    <a href="#" class="link-search"></a>
+                </li>
+                <li>
+                    <a href="#" class="link-bag"></a>
+                </li>
+            </ul>
+        </nav>
+
+<!-- end of navigation -->
+
+        <div class="search-container hide">
+            <div class="link-search">
+
+            </div>
+            <div class="search-bar">
+                <form action="">
+                    <input type="text" placeholder="Search your favourite mobile">
+                </form>
+            </div>
+            <div class="link-close">
+            </div>
+        </div>
+
+
+
+        <div class="mobile-search-container">
+            <div class="link-search">
+
+            </div>
+            <div class="search-bar">
+                <form action="">
+                    <input type="text" placeholder="Search your favourite mobile">
+                </form>
+            </div>
+        </div>
+    </div>
